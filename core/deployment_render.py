c0997ed40fc0d17cde013c741df095dd58b901a3
Oliver Santos Garnica <garnicavaldaoliversantos@gmail.com>
2025-12-24 09:30:07 -0400

    deployment_render

--- core/deployment_render.py
+++ core/deployment_render.py
@@ -3,11 +3,11 @@ import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['htpps://' + os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = False
-SECRET_KEY = os.environ.get['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
@@ -37,7 +37,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default= os.environ['DATABASE_URL'], 
+        default= os.environ('DATABASE_URL'), 
         conn_max_age=600
     )
 }
