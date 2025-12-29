277ff00f7fcf6a5a040a03b3c7c4049a133a749d
Oliver Santos Garnica <garnicavaldaoliversantos@gmail.com>
2025-12-29 11:24:42 -0400

    dejar de crear superuser

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-if [[ $CREATE_SUPERUSER ]]
+if [[ "$CREATE_SUPERUSER" == "True" ]];
 then
     python manage.py createsuperuser --no-input
 fi
